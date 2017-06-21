c98be4399a95916cf2e1ef8ead8eef838982449e
Mike Fiore <mfiore@multitech.com>
2017-06-21 12:48:05 -0500

    add MTS-Utils to setup scripts

--- setup.bat
+++ setup.bat
@@ -4,5 +4,9 @@ IF NOT EXIST ATParser (
     git clone http://github.com/ARMmbed/ATParser
 )
 
+IF NOT EXIST MTS-Utils (
+    git clone http://github.com/MultiTechSystems/MTS-Utils
+)
+
 version.bat
 
--- setup.sh
+++ setup.sh
@@ -5,5 +5,10 @@ then
     git clone http://github.com/ARMmbed/ATParser
 fi
 
+if [ ! -d MTS-Utils ]
+then
+    git clone http://github.com/MultiTechSystems/MTS-Utils
+fi
+
 ./version.sh
 
